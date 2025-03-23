48fcda49c81976dc09c982b92e6bafd19d3c89ea
Matheus Lacerda <matheus.l1996@gmail.com>
2025-03-22 22:42:55 -0300

    thumbnails and alphabetic order

--- skillpath/src/app/page.tsx
+++ skillpath/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Recommendation } from "./components/Recommendation";
-import { SimpleCard } from "./components/SimpleCard";
+import { Recommendation } from "../components/Recommendation";
+import { SimpleCard } from "../components/SimpleCard";
 
 export default function Home() {
   return (
@@ -7,9 +7,9 @@ export default function Home() {
       
       <header className="text-center pt-16 pb-12">
         <h1 className="text-7xl font-bold font-sans text-gray-900">Skillpath</h1>
-        <h2 className="text-2xl text-gray-600 mt-6 mb-8">Your road to get good at anything</h2>
+        <h2 className="text-2xl text-gray-600 mt-6 mb-8">Your roadmap to get good at anything</h2>
         <p className="text-gray-500 max-w-2xl mx-auto">
-          Skill Path is a collection of paved roads to learn anything and everything by yourself, with free online content
+          Skill Path is a collection of roadmaps to learn anything and everything by yourself
         </p>
       </header>
 
@@ -22,29 +22,21 @@ export default function Home() {
 
       <section className="mb-16">
         <h3 className="text-3xl font-semibold mb-2 text-gray-800">Courses</h3>
-        <p className="mb-6 text-gray-600 ">Master any subject whatsoever</p>
+        <p className="mb-6 text-gray-600 ">Master any skill whatsoever</p>
         <div className="grid grid-cols-4 gap-12">
-          <Recommendation link="/chess" title="Chess" subtitle="Learn the art of strategy" />
-          <Recommendation link="/negotiation" title="Negotiation" subtitle="Master the skill of persuasion" />
-          <Recommendation link="/business-entrepreneurship" title="Business/Entrepreneurship" subtitle="Start and grow a business" />
-          <Recommendation link="/bicycle" title="Bicycle" subtitle="All about bikes and maintenance" />
-          <Recommendation link="/programming" title="Programming" subtitle="Learn coding from scratch" />
-          <Recommendation link="/fullstack-development" title="Fullstack Development" subtitle="Frontend and backend development" />
-          <Recommendation link="/gamedev" title="Game Development" subtitle="Create your own video games" />
-          <Recommendation link="/architecture" title="Architecture" subtitle="Learn about building design" />
-          <Recommendation link="/bible" title="Bible" subtitle="Study the sacred scriptures" />
-          <Recommendation link="/gym" title="Gym" subtitle="Understand fitness and your muscles" />
-        </div>
-      </section>
-
-      <section className="pb-16">
-        <h3 className="text-3xl font-semibold mb-2 text-gray-800">Basics</h3>
-        <p className="mb-6 text-gray-600 ">Never miss out on a conversation</p>
-        <div className="grid grid-cols-4 gap-8">
-          <Recommendation link="/anime" title="Anime" subtitle="Understand the basics of anime." />
-          <Recommendation link="/classical-music" title="Classical Music" subtitle="Get started with classical music." />
-          <Recommendation link="/film-history" title="Film History" subtitle="Learn the essentials of film history." />
-          <Recommendation link="/fashion" title="Fashion" subtitle="Basics of global fashion trends." />
+          <Recommendation link="/courses/anime" title="Anime" subtitle="Know the rules of anime and the main shows" />
+          <Recommendation link="/courses/bible" title="Bible" subtitle="Study the sacred scriptures" />
+          <Recommendation link="/courses/chess" title="Chess" subtitle="Learn the art of strategy" />
+          <Recommendation link="/courses/entrepreneurship" title="Business/Entrepreneurship" subtitle="Start and grow a business" />
+          <Recommendation link="/courses/finance" title="Finance" subtitle="Money, money, money" />
+          <Recommendation link="/courses/fullstack-development" title="Fullstack Development" subtitle="Frontend and backend development" />
+          <Recommendation link="/courses/gamedev" title="Game Development" subtitle="Create your own video games" />
+          <Recommendation link="/courses/gym" title="Gym" subtitle="Understand fitness and your muscles" />
+          <Recommendation link="/courses/history" title="History" subtitle="Understand the context, how and why things happened" />
+          <Recommendation link="/courses/language" title="Language" subtitle="How to learn a language well?" />
+          <Recommendation link="/courses/movies" title="Movies" subtitle="Know all the movies, directors, and stories" />
+          <Recommendation link="/courses/music" title="Music" subtitle="Know all the singers, bands, and references" />
+          <Recommendation link="/courses/programming" title="Programming" subtitle="Learn coding from scratch" />
         </div>
       </section>
     </div>
