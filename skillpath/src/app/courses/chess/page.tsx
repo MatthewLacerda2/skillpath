0ea2c2d9721d4d4503f7d63aa0372f1cbc3f45c6
Matheus Lacerda <matheus.l1996@gmail.com>
2025-03-22 22:11:59 -0300

    rewriting things

---
+++ skillpath/src/app/courses/chess/page.tsx
@@ -0,0 +1,25 @@
+export default function Chess() {
+    return(
+        <div className="min-h-screen bg-gray-100 px-28 pt-14 pb-12">
+
+            <header className="text-center">
+                <h1 className="text-6xl font-bold font-sans text-gray-900 pb-2">Skillpath to Chess</h1>
+                <h2 className="text-2xl text-gray-600 mt-6 mb-14">Improve your decision-making</h2>
+                <p className="text-gray-500 max-w-2xl mx-auto pb-12">
+                    Skill Path is a collection of paved roads to learn anything and everything by yourself, with free online content
+                </p>
+            </header>            
+            
+            the book is on the table
+            
+            <iframe
+                width="720"
+                height="405"
+                src="https://www.youtube.com/embed/videoseries?si=wQf2djrE_0v3APTC&amp;list=PLDnx7w_xuguGl3y2Utxhp6eAKi9KhVlcx"
+                title="Mikhail Tal"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            />
+            
+        </div>
+    );
+}
